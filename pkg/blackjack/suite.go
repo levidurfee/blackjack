c5c0779f4c8bc694d741a4cd194b700b49bd8a80
Levi Durfee <levi.durfee@gmail.com>
2022-10-29 18:24:36 -0400

    Create suit consts

--- pkg/blackjack/suite.go
+++ pkg/blackjack/suite.go
@@ -1,5 +1,19 @@
 package blackjack
 
+const (
+	ClubsName    = "Clubs"
+	DiamondsName = "Diamonds"
+	HeartsName   = "Hearts"
+	SpadesName   = "Spades"
+)
+
+const (
+	ClubsSymbol    = "♣"
+	DiamondsSymbol = "♦"
+	HeartsSymbol   = "♥"
+	SpadesSymbol   = "♠"
+)
+
 type Suite struct {
 	Name   string
 	Symbol string
@@ -8,10 +22,10 @@ type Suite struct {
 type Suites []Suite
 
 var (
-	Clubs    = Suite{Name: "Clubs", Symbol: "♣"}
-	Diamonds = Suite{Name: "Diamonds", Symbol: "♦"}
-	Hearts   = Suite{Name: "Hearts", Symbol: "♥"}
-	Spades   = Suite{Name: "Spades", Symbol: "♠"}
+	Clubs    = Suite{Name: ClubsName, Symbol: ClubsSymbol}
+	Diamonds = Suite{Name: DiamondsName, Symbol: DiamondsSymbol}
+	Hearts   = Suite{Name: HeartsName, Symbol: HeartsSymbol}
+	Spades   = Suite{Name: SpadesName, Symbol: SpadesSymbol}
 )
 
 var SuitesSlice = Suites{
