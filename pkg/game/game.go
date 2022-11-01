a96e4e648ff1c37915fdd8d0398bd8aebbfe6c4d
Levi Durfee <levi.durfee@gmail.com>
2022-11-01 15:19:46 -0400

    Update game loops

--- pkg/game/game.go
+++ pkg/game/game.go
@@ -13,32 +13,33 @@ func Run() {
 	var cmd string
 	// Main loop for the game.
 	for {
-		// Loop for new hands.
+		fmt.Print(cls)
+		// Player's turn
 		for {
-			// Deal new hands
+			// Check if player bust
+			// Check if player has blackjack
 
-			// Show player hand and dealer face-up card
-
-			// Player's turn
-			for {
-				// Ask player what they wanna do
-				fmt.Scanln(&cmd)
-				if cmd == "s" {
-					break
-				}
+			// Show player their options
+			fmt.Println("hit, (s)tand, double")
+			fmt.Scanln(&cmd)
+			if cmd == "s" {
+				break
 			}
-
-			// Dealer's turn, this will be automated based on the dealer strategy
-			// for {
-
-			// }
 		}
-		// Ask if they want another hand or if they wanna quit.
+
+		// Dealer's turn
+		// for {
+		// Check if dealer bust
+		// Check if dealer has blackjack
+		// }
+
+		// Determine winner
+
+		fmt.Println("Another hand? y/n")
 		fmt.Scanln(&cmd)
-		switch cmd {
-		case "q":
+		if cmd == "n" {
 			return
 		}
-		// If they wanna keep playing, empty the current hands
+		// hand.Empty()
 	}
 }
