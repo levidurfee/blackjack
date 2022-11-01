4f1686b28f48e814a3e6009629cce3e679362116
Levi Durfee <levi.durfee@gmail.com>
2022-11-01 15:38:48 -0400

    loop

--- pkg/game/game.go
+++ pkg/game/game.go
@@ -10,36 +10,22 @@ func Run() {
 	// Setup stuff, welcome message, create players
 	fmt.Print(cls)
 
-	var cmd string
 	// Main loop for the game.
 	for {
-		fmt.Print(cls)
-		// Player's turn
+		// Print dealers hand
+
+		// Have player decide what they wanna do
 		for {
-			// Check if player bust
-			// Check if player has blackjack
 
-			// Show player their options
-			fmt.Println("hit, (s)tand, double")
-			fmt.Scanln(&cmd)
-			if cmd == "s" {
-				break
-			}
 		}
 
-		// Dealer's turn
-		// for {
-		// Check if dealer bust
-		// Check if dealer has blackjack
-		// }
+		// Let dealer decide what he wants to do
+		for {
 
-		// Determine winner
-
-		fmt.Println("Another hand? y/n")
-		fmt.Scanln(&cmd)
-		if cmd == "n" {
-			return
 		}
-		// hand.Empty()
+
+		// Ask if they wanna quit
+		//  return if they do
+		//  empty hands if they don't
 	}
 }
