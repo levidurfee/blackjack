24cf935063b74c54f47912fe739f664472c96b92
Levi Durfee <levi.durfee@gmail.com>
2022-11-02 08:47:31 -0400

    [game] get loops worked out

--- pkg/game/game.go
+++ pkg/game/game.go
@@ -1,31 +1,85 @@
 package game
 
-import "fmt"
+import (
+	"fmt"
 
-const (
-	cls = "\033[H\033[2J"
+	"git.x6c.co/go/blackjack/pkg/blackjack"
+	"git.x6c.co/go/blackjack/pkg/strategy"
 )
 
-func Run() {
-	// Setup stuff, welcome message, create players
+const (
+	cls       = "\033[H\033[2J"
+	handIndex = blackjack.DefaultHandIndex
+)
+
+var (
+	dealer = blackjack.NewDealer()
+	player = blackjack.NewPlayer("Levi")
+	deck   = blackjack.NewDecks(4)
+)
+
+func clear() {
 	fmt.Print(cls)
+}
 
-	// Main loop for the game.
+func deal() {
+	player.Hands = blackjack.NewHand()
+	dealer.Hands = blackjack.NewHand()
+
+	deck.Deal(&player.Hands[handIndex])
+	deck.Deal(&dealer.Hands[handIndex])
+
+	deck.Deal(&player.Hands[handIndex])
+	deck.Deal(&dealer.Hands[handIndex])
+}
+
+func printPlayers() {
+	clear()
+	fmt.Print(dealer)
+	fmt.Print(player)
+}
+
+func Run() {
+	var cmd string
 	for {
-		// Print dealers hand
-
-		// Have player decide what they wanna do
-		for {
-
+		deal()
+		printPlayers()
+		if !deck.HasEnoughCards() {
+			return
 		}
 
-		// Let dealer decide what he wants to do
+	actionloop:
 		for {
+			printPlayers()
+			if player.Hands[handIndex].Bust() {
+				break actionloop
+			}
+			if player.Hands[handIndex].IsBlackjack() {
+				break actionloop
+			}
+			action := strategy.Evaluate(player.Hands[handIndex], dealer.Hands[handIndex].DealerUpCard())
 
+			switch action {
+			case blackjack.Stand:
+				break actionloop
+			case blackjack.Hit:
+				deck.Deal(&player.Hands[handIndex])
+			case blackjack.Double:
+				deck.Deal(&player.Hands[handIndex])
+			}
 		}
 
-		// Ask if they wanna quit
-		//  return if they do
-		//  empty hands if they don't
+		// TODO: implement dealer strategy
+
+		winner := score(&player, &dealer)
+		if winner != nil {
+			fmt.Printf("%s won this hand\n", winner.Name)
+		}
+
+		fmt.Println("Another hand? y/n")
+		fmt.Scanln(&cmd)
+		if cmd == "n" {
+			break
+		}
 	}
 }
